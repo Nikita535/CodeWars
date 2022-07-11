3f8f5bbe29577df2e962294dd337e6c48bc54530
Nikita Zhiznevskiy <nzhigulevskiy@bk.ru>
2022-07-11 20:23:41 +0300

    NumberOfTrailingZeros

---
+++ src/com/company/Kyu5Medium/NumberOfTrailingZeros/Main.java
@@ -0,0 +1,16 @@
+package com.company.Kyu5Medium.NumberOfTrailingZeros;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(zeros(12));
+    }
+
+    public static int zeros(int n) {
+        int res=0;
+        while(n>0){
+            n/=5;
+            res+=n;
+        }
+        return res;
+    }
+}
